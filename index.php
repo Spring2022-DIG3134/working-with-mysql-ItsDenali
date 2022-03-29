8bbdd3b85fb18ba3e4a8d1ef36275e6c925701ee
Nate <DeepBlueFjords@Gmail.com>
2022-03-29 15:48:47 -0400

    Lab 8

--- index.php
+++ index.php
@@ -10,7 +10,44 @@
     <?php
        connect();
        salesTable();
+       updateTable();
        close();
     ?>
+
+    <!--Creates fields-->
+
+    <?php 
+    function updateTable() {
+        
+        echo("<form action='/index.php'>");
+
+        echo("<label for='customer_id'>First name:</label><br>");
+        echo("<input type='number' id='customer_id' name='customer_id'>");
+
+        echo("<br>");
+
+        echo("<label for='first_name'>Last name:</label><br>");
+        echo("<input type='text' id='first_name' name='first_name'>")
+
+        echo("<br>")
+
+        echo("<label for='last_name'>Last name:</label><br>");
+        echo("<input type='text' id='last_name' name='last_name'>");
+        
+        echo("<br>");
+
+        echo("<label for='city'>Last name:</label><br>");
+        echo("<input type='text' id='city' name='city'>");
+
+        echo("<br>");
+
+        echo("<label for='state'>Last name:</label><br>");
+        echo("<input type='text' id='state' name='state'>");
+
+        echo("<input type=-submit' value='Submit'>");
+
+        echo("</form>");
+    ?>
+
   </body>
 </html>
